dac16612de09710c19a18a572d25b421a1b53f16
opawina <nikol.shinkarenko@gmail.com>
2018-04-16 22:15:20 +0300

    orcwork

--- client.py
+++ client.py
@@ -6,11 +6,10 @@
 Beautiful code!
 '''
 
-import socket
-import argparse
 from time import time
 
 from Classes import Client
+from cli_handler import cli_handler
 
 
 def main():
@@ -36,25 +35,6 @@ def main():
             break
 
     sock.close()
-
-
-def cli_handler():
-
-    parser = argparse.ArgumentParser(description='Default host is 127.0.0.1:7777')
-
-    parser.add_argument('-a', '--addr', help='ip addres', default='127.0.0.1')
-    parser.add_argument('-p', '--port', help='tcp port', type=int, default=7777)
-
-    args = parser.parse_args()
-
-    # validating ip string
-    try:
-        socket.inet_aton(args.addr)
-    except:
-        print('ip address is not correct')
-        quit(3)
-
-    return (args.addr, args.port)
 
 
 
--- server.py
+++ server.py
@@ -6,11 +6,10 @@
 Beautiful code!
 '''
 
-import socket
-import argparse
 import json
 
 from Classes import Server
+from cli_handler import cli_handler
 
 # не знаю как использовать методы суперкласса в классе Server
 from Classes import JsonSocketConnector
@@ -62,24 +61,6 @@ def main():
             break
 
     sock.close()
-
-
-def cli_handler():
-    parser = argparse.ArgumentParser(description='Default host is 127.0.0.1:7777')
-
-    parser.add_argument('-a', '--addr', help='ip addres', default='127.0.0.1')
-    parser.add_argument('-p', '--port', help='tcp port', type=int, default=7777)
-
-    args = parser.parse_args()
-
-    # ip string validating
-    try:
-        socket.inet_aton(args.addr)
-    except:
-        print('ip address is not correct')
-        quit(3)
-
-    return (args.addr, args.port)
 
 
 def useful_work(conn):
